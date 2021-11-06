ce36770043487133afb34c1a0f4fe9ff84d10934
obr-note <k33mdukb9n@yahoo.co.jp>
2021-11-06 20:28:54 +0900

    add test post index

--- todoapp_test.go
+++ todoapp_test.go
@@ -2,14 +2,17 @@ package todoapp_test
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/obrkn/todoapp/pkg"
 )
 
-func TestIndex(t *testing.T) {
+func TestGetIndex(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "localhost:8080", nil)
 
 	got := httptest.NewRecorder()
@@ -21,6 +24,25 @@ func TestIndex(t *testing.T) {
 	fmt.Println(got.Code)
 }
 
-// func TestPost(t *testing.T) {
-// 	req :=
-// }
+func TestPostIndex(t *testing.T) {
+	req := httptest.NewRequest(http.MethodPost, "localhost:8080", nil)
+
+	rand.Seed(time.Now().UnixNano())
+	testNum := fmt.Sprint("テスト番号: ", rand.Intn(100000))
+	q := req.URL.Query()
+	q.Add("content", testNum)
+	req.URL.RawQuery = q.Encode()
+
+	got := httptest.NewRecorder()
+	pkg.Index(got, req)
+
+	if got.Code != http.StatusOK {
+		t.Errorf("not status OK")
+	}
+	fmt.Println(got.Code)
+
+	if got := got.Body.String(); !strings.Contains(got, testNum) {
+		t.Errorf("not match")
+	}
+	fmt.Println(testNum)
+}

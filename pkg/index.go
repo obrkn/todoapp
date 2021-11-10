1d0734e88f71bb4db223f1ca71987c6e283f28eb
obr-note <k33mdukb9n@yahoo.co.jp>
2021-11-10 20:45:07 +0900

    complete app

--- pkg/index.go
+++ pkg/index.go
@@ -18,7 +18,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	} else if env == "test" {
 		db, err = sql.Open("mysql", "root@tcp(127.0.0.1)/todoapp?parseTime=true&loc=Asia%2FTokyo")
 	} else {
-		db, err = sql.Open("mysql", "root@tcp(db)/todoapp?parseTime=true&loc=Asia%2FTokyo")
+		db, err = sql.Open("mysql", "root@tcp(127.0.0.1)/todoapp?parseTime=true&loc=Asia%2FTokyo")
 	}
 	if err != nil {
 		log.Fatal(err)
